06db6a82de7a6223268fa323210559b68db6af75
Joren Van Onder <joren@jvo.sh>
2021-02-08 22:04:20 -0800

    Refactor

--- main.py
+++ main.py
@@ -29,25 +29,36 @@ def generate_interval(semitones) -> Tuple[Note, Note]:
     return Note(NOTES[root], octave), Note(NOTES[extra], octave_extra)
 
 
-mixer = Mixer(44100, 0.5)
-mixer.create_track(0, SINE_WAVE, attack=0.01, decay=0.1)
-mixer.create_track(1, SINE_WAVE, attack=0.01, decay=0.1)
+def setup_mixer():
+    mixer = Mixer(44100, 0.5)
+    mixer.create_track(0, SINE_WAVE, attack=0.01, decay=0.1)
+    mixer.create_track(1, SINE_WAVE, attack=0.01, decay=0.1)
+    return mixer
 
-DURATION = 4
-INTERVALS = 4
-INTERVAL_SEMITONES = 4
 
-for _ in range(INTERVALS):
-    note_a, note_b = generate_interval(INTERVAL_SEMITONES)
-    mixer.add_note(0, note=note_a.name, octave=note_a.octave, duration=DURATION)
-    mixer.add_note(1, note=note_b.name, octave=note_b.octave, duration=DURATION)
-    mixer.add_silence(0, 0.1)
-    mixer.add_silence(1, 0.1)
+def generate_intervals(mixer, interval_semitones: int):
+    duration_s = 4
+    amount = 4
 
-WAVE_FILE = "/tmp/tones.wav"
-mixer.write_wav(WAVE_FILE)
+    for _ in range(amount):
+        note_a, note_b = generate_interval(interval_semitones)
+        mixer.add_note(0, note=note_a.name, octave=note_a.octave, duration=duration_s)
+        mixer.add_note(1, note=note_b.name, octave=note_b.octave, duration=duration_s)
+        mixer.add_silence(0, 0.1)
+        mixer.add_silence(1, 0.1)
 
-playsound(WAVE_FILE)
+
+def write_and_play(mixer):
+    wave_file = "/tmp/tones.wav"
+    mixer.write_wav(wave_file)
+    playsound(wave_file)
+
+
+def run():
+    mixer = setup_mixer()
+    generate_intervals(mixer, 4)
+    write_and_play(mixer)
+
 
 if __name__ == "__main__":
-    pass
+    run()
